46e1e903cd776903cdb20e1d6ef0e5cae2b55080
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-14 17:43:50 +0200

    fix in repo

--- src/main/java/com/durys/jakub/personalrequestsservice/requestypes/domain/RequestTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.durys.jakub.personalrequestsservice.requestypes.domain;
-
-import java.util.Optional;
-import java.util.Set;
-
-public interface RequestTypeRepository {
-    Optional<RequestType> load(Long id);
-    Set<RequestTypeField> requestFields(Long id);
-}
---
+++ src/main/java/com/durys/jakub/personalrequestsservice/requestypes/infrastructure/RequestTypeRepository.java
@@ -0,0 +1,15 @@
+package com.durys.jakub.personalrequestsservice.requestypes.infrastructure;
+
+import com.durys.jakub.personalrequestsservice.requestypes.domain.RequestType;
+import com.durys.jakub.personalrequestsservice.requestypes.domain.RequestTypeField;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
+
+public interface RequestTypeRepository extends JpaRepository<RequestType, Long> {
+
+    @Query("select t.fields from RequestType t where t.id = :requestTypeId")
+    Set<RequestTypeField> fields(Long requestTypeId);
+
+}
