e54d40d72b83447ca9e32775be59852686a7fcc7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-14 16:22:55 +0200

    init

---
+++ src/main/java/com/durys/jakub/personalrequestsservice/PersonalRequestsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.personalrequestsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalRequestsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalRequestsServiceApplication.class, args);
+	}
+
+}
