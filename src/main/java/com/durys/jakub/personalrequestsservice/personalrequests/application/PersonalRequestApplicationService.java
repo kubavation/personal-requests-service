0f8c0e27540ea8145cf17773d11076f39d8106b3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-14 18:14:26 +0200

    init of PersonalRequestApplicationService

---
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/application/PersonalRequestApplicationService.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.personalrequestsservice.personalrequests.application;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonalRequestApplicationService {
+}
