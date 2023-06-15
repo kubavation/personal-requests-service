288a24bf359f257a3317d3fc04bf8ff79b2c4024
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-15 20:50:02 +0200

    tenantRequest - mock method

--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/PersonalRequestConstructOperations.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/PersonalRequestConstructOperations.java
@@ -4,8 +4,12 @@ import com.durys.jakub.personalrequestsservice.personalrequests.domain.Requestab
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequestMapping("/personal-requests")
 public interface PersonalRequestConstructOperations<T extends Requestable> {
+
+    List<T> tenantRequests(String tenantId);
 
     @PostMapping
     void save(T t);
--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/contexts/test/TestPersonalRequestConstructController.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/contexts/test/TestPersonalRequestConstructController.java
@@ -7,10 +7,17 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @ConditionalOnProperty(prefix = "application", name = "context", havingValue = "TEST")
 public class TestPersonalRequestConstructController implements PersonalRequestConstructOperations<TestPersonalRequest> {
+
+    @Override
+    public List<TestPersonalRequest> tenantRequests(String tenantId) {
+        return List.of();
+    }
 
     @Override
     public void save(@RequestBody TestPersonalRequest testPersonalRequest) {
