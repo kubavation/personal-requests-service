8446d3abbd96cdf0a494f121e1d34b5123261c63
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-16 18:31:25 +0200

    clearing

--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/application/PersonalRequestApplicationService.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/application/PersonalRequestApplicationService.java
@@ -1,10 +1,6 @@
 package com.durys.jakub.personalrequestsservice.personalrequests.application;
 
-import com.durys.jakub.personalrequestsservice.context.Context;
 import com.durys.jakub.personalrequestsservice.context.ContextProvider;
-import com.durys.jakub.personalrequestsservice.personalrequests.domain.AbstractPersonalRequestFactory;
-import com.durys.jakub.personalrequestsservice.personalrequests.domain.Requestable;
-import com.durys.jakub.personalrequestsservice.requestypes.domain.Type;
 import com.durys.jakub.personalrequestsservice.requestypes.infrastructure.RequestTypeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +14,5 @@ public class PersonalRequestApplicationService {
     private final ContextProvider contextProvider;
     private final RequestTypeRepository requestTypeRepository;
 
-    public Requestable patternFor(Type type) {
 
-        Context context = contextProvider.context();
-
-        log.info("context {}", context.name());
-
-        return AbstractPersonalRequestFactory
-                .instanceFrom(context, type, requestTypeRepository);
-    }
 }
--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/PersonalRequestController.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/infrastructure/in/PersonalRequestController.java
@@ -1,29 +1,23 @@
 package com.durys.jakub.personalrequestsservice.personalrequests.infrastructure.in;
 
 import com.durys.jakub.personalrequestsservice.personalrequests.application.PersonalRequestApplicationService;
-import com.durys.jakub.personalrequestsservice.personalrequests.domain.PersonalRequest;
-import com.durys.jakub.personalrequestsservice.requestypes.domain.Type;
+import com.durys.jakub.personalrequestsservice.personalrequests.infrastructure.model.Request;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @Slf4j
 @RestController
-@RequestMapping("/personal-requests")
+@RequestMapping("/personal-requests-details")
 @RequiredArgsConstructor
 public class PersonalRequestController {
 
     private final PersonalRequestApplicationService personalRequestApplicationService;
 
-    @GetMapping("/{type}/pattern")
-    PersonalRequest pattern(@PathVariable Type type) {
-        log.info("pattern for {}", type.name());
-        return personalRequestApplicationService.patternFor(type);
+    @PostMapping
+    void save(@RequestBody Request personalRequest) {
     }
 }
