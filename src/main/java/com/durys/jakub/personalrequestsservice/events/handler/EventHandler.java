af56a9f31239e34de3ea1213748bf8f98c95d15b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-20 18:23:15 +0200

    eventHandler annotation

---
+++ src/main/java/com/durys/jakub/personalrequestsservice/events/handler/EventHandler.java
@@ -0,0 +1,14 @@
+package com.durys.jakub.personalrequestsservice.events.handler;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface EventHandler {
+}
