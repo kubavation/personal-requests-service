1156b5c2807ed1edfb08e8128437a618222bda55
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-20 20:15:08 +0200

    PersonalRequestCirculationHistory

---
+++ src/main/java/com/durys/jakub/personalrequestsservice/requestcirculationhistory/domain/PersonalRequestCirculationHistory.java
@@ -0,0 +1,23 @@
+package com.durys.jakub.personalrequestsservice.requestcirculationhistory.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "PR_PERSONAL_REQUEST_HISTORY")
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class PersonalRequestCirculationHistory {
+
+    @Id
+    @Column(name = "REQUEST_ID")
+    private Long requestId;
+
+    private String tenantId;
+
+    private String history;
+
+}
