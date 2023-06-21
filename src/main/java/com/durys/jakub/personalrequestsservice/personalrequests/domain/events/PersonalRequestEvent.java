6cfe3db81b373ab625f6076de4fa87fa9e5f071e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-21 21:21:09 +0200

    status() method in personalRequestEvent interface

--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestEvent.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestEvent.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.personalrequestsservice.personalrequests.domain.events;
 
 import com.durys.jakub.personalrequestsservice.events.DomainEvent;
+import com.durys.jakub.personalrequestsservice.personalrequests.domain.PersonalRequestStatus;
 
 public interface PersonalRequestEvent extends DomainEvent {
+    PersonalRequestStatus status();
 }
--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestStatusChangedEvent.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestStatusChangedEvent.java
@@ -1,6 +1,5 @@
 package com.durys.jakub.personalrequestsservice.personalrequests.domain.events;
 
-import com.durys.jakub.personalrequestsservice.events.DomainEvent;
 import com.durys.jakub.personalrequestsservice.personalrequests.domain.PersonalRequestStatus;
 
 public record PersonalRequestStatusChangedEvent(Long requestId, String supervisorId, PersonalRequestStatus status) implements PersonalRequestEvent {
