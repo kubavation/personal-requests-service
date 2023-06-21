b0a1cff8b77a1ef7760401cc22429a9dfb58264b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-21 20:50:45 +0200

    additional query

--- src/main/java/com/durys/jakub/personalrequestsservice/requestcirculationhistory/infrastructure/SupervisorAcceptationHistoryRepository.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/requestcirculationhistory/infrastructure/SupervisorAcceptationHistoryRepository.java
@@ -4,10 +4,13 @@ import com.durys.jakub.personalrequestsservice.requestcirculationhistory.domain.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SupervisorAcceptationHistoryRepository extends JpaRepository<SupervisorAcceptationHistory, Long> {
 
     @Query("from SupervisorAcceptationHistory h where h.requestId = :requestId and h.supervisorId = :supervisorId")
     Optional<SupervisorAcceptationHistory> findBySupervisorAndRequest(String supervisorId, String requestId);
+
+    List<SupervisorAcceptationHistory> findByRequestId(Long requestId);
 }
