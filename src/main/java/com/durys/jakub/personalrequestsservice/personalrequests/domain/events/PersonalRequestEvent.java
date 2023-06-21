e61a218f580b62f253462c2834939922bf045c75
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-21 21:19:09 +0200

    personalRequestEvent interface + changes in events

--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestEvent.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestEvent.java
@@ -2,5 +2,5 @@ package com.durys.jakub.personalrequestsservice.personalrequests.domain.events;
 
 import com.durys.jakub.personalrequestsservice.events.DomainEvent;
 
-public record PersonalRequestCreatedEvent(Long requestId, String authorId) implements PersonalRequestEvent {
+public interface PersonalRequestEvent extends DomainEvent {
 }
--- src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestStatusChangedEvent.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/events/PersonalRequestStatusChangedEvent.java
@@ -3,5 +3,5 @@ package com.durys.jakub.personalrequestsservice.personalrequests.domain.events;
 import com.durys.jakub.personalrequestsservice.events.DomainEvent;
 import com.durys.jakub.personalrequestsservice.personalrequests.domain.PersonalRequestStatus;
 
-public record PersonalRequestStatusChangedEvent(Long requestId, String supervisorId, PersonalRequestStatus status) implements DomainEvent {
+public record PersonalRequestStatusChangedEvent(Long requestId, String supervisorId, PersonalRequestStatus status) implements PersonalRequestEvent {
 }
