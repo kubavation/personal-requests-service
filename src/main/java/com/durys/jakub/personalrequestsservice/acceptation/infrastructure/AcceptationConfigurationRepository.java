4ea99b5cbec2094f74c8d12b112ea5f9003587ab
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-28 19:43:44 +0200

    fix

--- src/main/java/com/durys/jakub/personalrequestsservice/acceptation/infrastructure/AcceptationConfigurationRepository.java
+++ src/main/java/com/durys/jakub/personalrequestsservice/acceptation/infrastructure/AcceptationConfigurationRepository.java
@@ -1,6 +1,7 @@
 package com.durys.jakub.personalrequestsservice.acceptation.infrastructure;
 
 import com.durys.jakub.personalrequestsservice.acceptation.domain.AcceptationConfiguration;
+import com.durys.jakub.personalrequestsservice.acceptation.infrastructure.model.Supervisor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +12,7 @@ public interface AcceptationConfigurationRepository extends JpaRepository<Accept
     @Query(value = "SELECT config.supervisor_id supervisorId from PR_ACCEPTATION_CONFIGURATION config " +
             " WHERE current_date between config.from and COALESCE(config.to, current_date) " +
             " AND config.level = :level AND config.tenant_id = :tenantId and config.status = 'A' ", nativeQuery = true)
-    Optional<AcceptationConfiguration> supervisor(String tenantId, Integer level);
+    Optional<Supervisor> supervisor(String tenantId, Integer level);
 
     @Query(value = "SELECT max(config.level) from PR_ACCEPTATION_CONFIGURATION config " +
             " WHERE current_date between config.from and COALESCE(config.to, current_date) " +
