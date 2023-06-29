da7d7d2827e1136352877d4308891374bcf20d9c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-06-29 19:19:29 +0200

    acceptationLevel model

---
+++ src/main/java/com/durys/jakub/personalrequestsservice/personalrequests/domain/AcceptationLevel.java
@@ -0,0 +1,24 @@
+package com.durys.jakub.personalrequestsservice.personalrequests.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class AcceptationLevel {
+    private Integer level = 0;
+
+    AcceptationLevel reset() {
+        this.level = 0;
+        return this;
+    }
+
+    AcceptationLevel increment() {
+        this.level++;
+        return this;
+    }
+}
